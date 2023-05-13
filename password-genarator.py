0717cb5f41b368f1183c987bbd407a728f8de4ab
Finn Everspaugh <finn.everspaugh@gmail.com>
2023-05-12 19:24:52 -0500

    update

--- password-genarator.py
+++ password-genarator.py
@@ -12,30 +12,23 @@ def store_password(name, whatfor, password):
     with open(f'{sys.path[0]}/passwords.txt', 'a') as file:
         file.write(text)
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/run_it', methods=['GET'])
-def run_it() -> str:
+def run_it():
     name = request.args.get('name')
     whatfor = request.args.get('whatfor')
     characters = request.args.get('characters')
+    password = create_password(int(characters))
+    store_password(name,whatfor,password)
+    result = {
+        "password": password
+    }
+    return jsonify(result)
 
-    password = create_password(charecters_long=characters)
-
-    store_password(name=name, whatfor=whatfor, password=password)
-
-    response = make_response(password)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-if __name__ == "__main__":
-    app.run(port=8000)
-    # name = input("What is your name?: ")
-    # whatfor = input('What is this password for?: ')
-    # charecters = input("How long whould you like your password to be?: ")
-    # password = create_password(charecters_long=charecters)
-    # print(f"Ok {name.title()}. Your password for {whatfor} is {password}")
-    # store_password(name=name, whatfor=whatfor, password=password)
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
